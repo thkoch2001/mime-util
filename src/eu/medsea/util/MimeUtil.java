f3f9beb64659324adc153fc3d5be6e8aec0a3587
Steven McArdle <smcardle@medsea.eu>
2009-01-20 21:58:31 +0000

    Removed the conf and docs directories Changed some javadoc comments Modified the ant build files

--- src/eu/medsea/util/MimeUtil.java
+++ src/eu/medsea/util/MimeUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007-2009 Medsea Business Solutions S.L.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.medsea.util;
 
 import java.io.BufferedReader;
@@ -28,6 +43,70 @@ import org.apache.commons.logging.LogFactory;
 import eu.medsea.util.MagicMimeEntry;
 import eu.medsea.util.MimeException;
 
+/**
+ * <p>
+ * The <code>MimeUtil</code> utility class is used to detect mime types from either a files extension
+ * or by looking into the file at various offsets and comparing or looking for certain values in the same way
+ * as the Unix <code>file(1)</code> command.
+ * </p>
+ * <p>
+ * It is important to note that mime matching is not an exact science meaning that a positive match does not guarantee the returned mime type
+ * is correct. It is only a best guess method of matching and should be used with this in mind.
+ * </p>
+ * <p>
+ * Both the file extension mapping and the magic mime number rules can be extended by the user.
+ * </p>
+ * <p>
+ * The extension mime mappings are loaded in the following way.
+ * <ol>
+ * <li>Load the properties file from the mime utility jar named <code>eu.medsea.mime.mime-types.properties</code>.</li>
+ * <li>Locate and load a file named <code>.mime-types.properties</code> from the users home directory if one exists.</li>
+ * <li>Locate and load a file named <code>mime-types.properties</code> from the classpath if one exists</li>
+ * <li>locate and load a file named by the JVM property <code>mime-mappings</code> i.e. <code>-Dmime-mappings=../my-mime-types.properties</code></li>
+ * </ol>
+ * Each property file loaded will add to the list of extensions. If there is a clash of extension names then the last one loaded wins,
+ * this makes it possible to completely change the mime types associated to a file extension declared in previous property files.
+ * </p>
+ * <p>
+ * We acquired many mappings from many different sources on the net for the extension mappings. The internal list is quite large and there can
+ * be many associated mime types. These may not match what you are expecting so you can add the mapping you want to change to your own property
+ * file following the rules above. If you provide a mapping for an extension then any previously loaded mappings will be removed and only the
+ * mappings you define will be returned. This can be used to map certain extensions that are incorrectly returned for our environment defined
+ * in the internal property file.
+ * </p>
+ * <p>
+ * If we have not provided a mapping for a file extension that you know the mime type for you can add this to your custom property file
+ * so that a correct mime type is returned for you.
+ * <p>
+ * The magic mime rules files are loaded in the following way.
+ * <ol>
+ * <li>From a JVM system property <code>magic-mime</code> i.e <code>-Dmagic-mime=../my/magic/mime/rules</code></li>
+ * <li>From any file named <code>magic.mime</code> that can be found on the classpath</li>
+ * <li>From a file named <code>.magic.mime</code> in the users home directory</li>
+ * <li>From the normal Unix locations <code>/usr/share/file/magic.mime</code> and <code>/etc/magic.mime</code> (in that order)</li>
+ * <li>From the internal <code>magic.mime</code> file <code>eu.medsea.mime.magic.mime</code> if, and only if, no files are located in step 4 above.</li>
+ * </ol>
+ * Each rule file is appended to the end of the existing rules so the earlier in the sequence you define a rule means this will take precedence
+ * over rules loaded later.
+ * </p>
+ * <p>
+ * As with the extension mappings you can add new mime mapping rules using the syntax defined for the Unix magic.mime file by placing these rules
+ * in any of the files or locations listed above. You can also change an existing mapping rule by redefining the existing rule in one of the files
+ * listed above. This is handy for some of the more sketchy rules defined in the existing Unix magic.mime files.
+ * </p>
+ * <p>
+ * When using the utility methods we always try to return a mime type even if no mapping can be found. In the case of no mapping we have defined
+ * the utility to return <code>application/octet-stream</code> by default. This can be overridden and you can have a no-match return any mime type you
+ * define, even unofficial mime types that you made up just for your application such as <code>application/x-unknown-mime-type</code>. This allows
+ * your application to provide special handling on a no-match such as executing a custom business process.
+ * </p>
+ * <p>
+ * We use the <code>application/directory</code> mime type to identify directories. Even though this is not an official mime type
+ * it seems to be well accepted on the net as an unofficial mime type so we thought it was OK for us to use as well.
+ * </p>
+ * @author Steven McArdle.
+ *
+ */
 public class MimeUtil
 {
 	private static Log log = LogFactory.getLog(MimeUtil.class);
@@ -77,9 +156,9 @@ public class MimeUtil
 	 * Get the mime type of the data in the specified {@link InputStream}. Therefore,
 	 * the <code>InputStream</code> must support mark and reset
 	 * (see {@link InputStream#markSupported()}). If it does not support mark and reset,
-	 * an {@link IllegalArgumentException} is thrown.
+	 * an {@link MimeException} is thrown.
 	 *
-	 * @param the stream from which to read the data.
+	 * @param in the stream from which to read the data.
 	 * @return the mime type. Never returns <code>null</code> (if the mime type cannot be found, {@link #UNKNOWN_MIME_TYPE} is returned).
 	 * @throws MimeException if the specified <code>InputStream</code> does not support mark and reset (see {@link InputStream#markSupported()}).
 	 */
@@ -689,23 +768,6 @@ public class MimeUtil
 		initMagicRules();
 	}
 
-	public static void main(String [] args) throws IOException {
-		System.out.println(mMagicMimeEntries);
-
-		File dir = null;
-		if(args.length == 0) {
-			dir = new File(".");
-		} else {
-			dir = new File(args[0]);
-		}
-		File [] f = dir.listFiles();
-		for(int i = 0; i < f.length; i++) {
-			System.out.println("(Using name) file : " + f[i].getCanonicalPath() + " : mimeType : " + MimeUtil.getMimeType(f[i].getCanonicalPath()));
-			System.out.println("(Using file) file : " + f[i].getCanonicalPath() + " : mimeType : " + MimeUtil.getMimeType(f[i]));
-			System.out.println("-----------------------");
-		}
-	}
-
 	// Parse the magic.mime file
     private static void parse(String magicFile, Reader r) throws IOException {
     	long start = System.currentTimeMillis();
@@ -859,7 +921,7 @@ public class MimeUtil
 
 	/**
 	 * Get the mime type of a file using the <code>magic.mime</code> rules files.
-	 * @param f is a file object that points to a file or directory.
+	 * @param file is a {@link File} object that points to a file or directory.
  	 * @return the mime type. Never returns <code>null</code> (if the mime type cannot be found, {@link #UNKNOWN_MIME_TYPE} is returned).
 	 * @throws MimeException if the file cannot be parsed.
 	 */
